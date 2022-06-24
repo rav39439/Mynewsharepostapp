147745004791a276728ad0824efa241359116115
rav3434 <ravish_cg@yahoo.com>
2022-06-24 10:26:35 +0530

    new commit

--- src/main/java/com/mynewapplication/web/config/CustomUserDetails.java
+++ src/main/java/com/mynewapplication/web/config/CustomUserDetails.java
@@ -3,6 +3,7 @@ package com.mynewapplication.web.config;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,9 +24,11 @@ public class CustomUserDetails implements UserDetails {
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 
 		SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole());
-		return List.of(simpleGrantedAuthority);
-		
-		
+
+		List<SimpleGrantedAuthority> mylist = new ArrayList<>();
+		mylist.add(simpleGrantedAuthority);
+		return mylist;
+
 		// return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
 
 	}
